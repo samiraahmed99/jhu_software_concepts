04a00e6cb4c86aa88dae4bced4dbe1273e23dc35
samiraahmed <sahme102@jhu.edu>
2025-05-21 22:30:19 -0700

    bare bones structure for website

---
+++ Module_1/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+# import creat_app() function from __init__.py located in our web application module
+from samira_web_app import create_app()
+# create a Flask application instance
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 8000, debug = True)
