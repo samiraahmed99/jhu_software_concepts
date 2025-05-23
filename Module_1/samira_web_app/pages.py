dc9c7934ec8e3a48aaa9dbb7960bb5ee50371b80
samiraahmed <sahme102@jhu.edu>
2025-05-22 18:10:21 -0700

    removed this it was a test

--- Module_1/samira_web_app/pages.py
+++ Module_1/samira_web_app/pages.py
@@ -5,10 +5,6 @@ from flask import Blueprint, render_template, url_for
 bp = Blueprint('pages', __name__)
 
 # use pages/ name of each page since they are in a 'pages' folder to separate my template html
-@bp.route('/debug')
-def debug():
-    return "Blueprint is active"
-
 @bp.route('/')
 def home():
     return render_template('pages/home.html', page = 'home')
