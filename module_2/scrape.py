d69ac392f5d43b31dc54178734b8583ad134f5b4
samiraahmed <sahme102@jhu.edu>
2025-05-31 00:45:24 -0700

    removed a print statement I used to check correct entries

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,32 +1,6 @@
-# Confirm robo.txt file permits scraping
-import urllib.robotparser
 import requests
 from bs4 import BeautifulSoup
 import re
-
-# Initialize RobotFileParser
-rp = urllib.robotparser.RobotFileParser()
-rp.set_url("https://www.thegradcafe.com/robots.txt")
-rp.read()
-
-agent = "samira"
-base_path = "https://www.thegradcafe.com/survey/?page="
-
-# Check access for each page only print if a specific page is disallowed
-# There are 20 entries per page, so 500 pages total to get 10,000 entries
-disallowed_pages = []
-for page_num in range(1, 501):
-    full_url = f"{base_path}{page_num}"
-    if not rp.can_fetch(agent, full_url):
-        disallowed_pages.append(full_url)
-
-if len(disallowed_pages) > 0:
-    print(f"{len(disallowed_pages)} page(s) are disallowed:")
-    for url in disallowed_pages:
-        print(url)
-else:
-    print("All 500 survey pages are allowed for scraping.")
-
 
 # Scrape Data:
 class ScrapeData:
@@ -157,4 +131,3 @@ class ScrapeData:
 if __name__ == "__main__":
     scraper = ScrapeData()
     data = scraper.scrape_pages(start=1, end=500)
-    print(f"Total entries scraped: {len(data)}")
